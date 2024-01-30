2e93f3917861f6e589f061087608289ec68dfa11
0xKitsune <0xKitsune@protonmail.com>
2024-01-30 10:52:27 -0500

    added denominator computation

--- src/coordinator.rs
+++ src/coordinator.rs
@@ -12,13 +12,13 @@ use memmap::{Mmap, MmapOptions};
 use serde::{Deserialize, Serialize};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
-use tokio::sync::mpsc;
+use tokio::sync::{self, mpsc};
 use tokio::task::JoinHandle;
 
 use crate::bits::Bits;
 use crate::distance::{self, MasksEngine};
 use crate::encoded_bits::EncodedBits;
-use crate::template::Template;
+use crate::template::{self, Template};
 
 const BATCH_SIZE: usize = 20_000;
 
@@ -59,39 +59,28 @@ impl Coordinator {
 
     //TODO: update error handling
     pub async fn spawn(mut self) -> eyre::Result<()> {
-        let mmap_db: Arc<_> = Arc::new(self.initialize_mmap_db());
+        let mmap_db: Arc<Mmap> = Arc::new(self.initialize_mmap_db());
 
         loop {
-            if let Some(messages) = self.dequeue_shares().await? {
+            if let Some(messages) = self.dequeue_queries().await? {
                 for message in messages {
-                    let template: Template = serde_json::from_str(
+                    let template = serde_json::from_str::<Template>(
                         //TODO: handle this error
                         &message.body.expect("No body in message"),
                     )?;
 
-                    // Write each share to the corresponding participant
-                    future::try_join_all(self.participants.iter_mut().map(
-                        |stream| async move {
-                            // Send query
-                            stream
-                                .write_all(bytemuck::bytes_of(&template))
-                                .await
-                        },
-                    ))
-                    .await?;
+                    self.send_query_to_participants(&template).await?;
 
-                    // let (sender, mut receiver) = mpsc::channel(4000);
-                    // let denominator_worker =
-                    //     tokio::task::spawn_blocking(move || {
-                    //         let masks: &[Bits] = bytemuck::cast_slice(&mmap_db);
-                    //         let engine = MasksEngine::new(&template.mask);
-                    //         for chunk in masks.chunks(BATCH_SIZE) {
-                    //             let mut result = vec![[0_u16; 31]; chunk.len()];
-                    //             engine.batch_process(&mut result, chunk);
-                    //             sender.blocking_send(result)?;
-                    //         }
-                    //         Ok(())
-                    //     });
+                    let mut handles = vec![];
+
+                    let (denominator_handle, denominator_rx) = self
+                        .compute_denominators(mmap_db.clone(), template.mask);
+
+                    handles.push(denominator_handle);
+
+                    //TODO: handle participant shares
+
+                    //TODO:
                 }
             }
 
@@ -99,11 +88,53 @@ impl Coordinator {
         }
     }
 
+    pub async fn send_query_to_participants(
+        &mut self,
+        query: &Template,
+    ) -> eyre::Result<()> {
+        // Write each share to the corresponding participant
+        future::try_join_all(self.participants.iter_mut().map(
+            |stream| async move {
+                // Send query
+                stream.write_all(bytemuck::bytes_of(query)).await
+            },
+        ))
+        .await?;
+        Ok(())
+    }
+
+    pub fn compute_denominators(
+        &self,
+        mmap_db: Arc<Mmap>,
+        mask: Bits,
+    ) -> (
+        tokio::sync::mpsc::Receiver<Vec<[u16; 31]>>,
+        JoinHandle<eyre::Result<()>>,
+    ) {
+        let (sender, denom_receiver) = tokio::sync::mpsc::channel(4);
+        let denominator_handle = tokio::task::spawn_blocking(move || {
+            let masks: &[Bits] = bytemuck::cast_slice(&mmap_db);
+            let engine = MasksEngine::new(&mask);
+            for chunk in masks.chunks(BATCH_SIZE) {
+                let mut result = vec![[0_u16; 31]; chunk.len()];
+                engine.batch_process(&mut result, chunk);
+                sender.blocking_send(result)?;
+            }
+            Ok(())
+        });
+
+        (denom_receiver, denominator_handle)
+    }
+
+    pub fn collect_participant_shares(&self) -> JoinHandle<eyre::Result<()>> {
+        todo!()
+    }
+
     pub fn initialize_mmap_db(&self) -> Mmap {
         todo!()
     }
 
-    pub async fn dequeue_shares(
+    pub async fn dequeue_queries(
         &self,
     ) -> eyre::Result<Option<Vec<aws_sdk_sqs::types::Message>>> {
         let messages = self
@@ -119,13 +150,15 @@ impl Coordinator {
 
     //TODO: update error handling
     pub async fn enqueue_distance_shares(&self) -> eyre::Result<()> {
-        let distances_queue = self
-            .aws_client
-            .send_message()
-            .queue_url(self.distances_queue_url.clone())
-            // .message_body(input) //TODO: update/uncomment this
-            .send()
-            .await?;
+        todo!();
+
+        // let distances_queue = self
+        //     .aws_client
+        //     .send_message()
+        //     .queue_url(self.distances_queue_url.clone())
+        //     // .message_body(input) //TODO: update/uncomment this
+        //     .send()
+        //     .await?;
 
         Ok(())
     }
