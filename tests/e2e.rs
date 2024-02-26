6e11b15b93b029b18f93c6114eb7fdbe9917d837
0xKitsune <0xKitsune@protonmail.com>
2024-02-26 13:26:10 -0500

    updated resource initialization

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,86 +1,176 @@
 use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
 
 use aws_sdk_sqs::operation::get_queue_attributes::GetQueueAttributes;
 use aws_sdk_sqs::types::QueueAttributeName;
-use mpc::config::CoordinatorConfig;
+use config::{Config, FileFormat};
+use mpc::config::{AwsConfig, CoordinatorConfig, ParticipantConfig};
 use mpc::coordinator::Coordinator;
+use mpc::db::Db;
+use mpc::participant::Participant;
 use mpc::utils::aws::sqs_client_from_config;
-use testcontainers::{clients, RunnableImage};
+use serde::Deserialize;
+use testcontainers::core::WaitFor;
+use testcontainers::{clients, Container, GenericImage, Image, RunnableImage};
 use testcontainers_modules::localstack::LocalStack;
 use testcontainers_modules::postgres::{self, Postgres};
+use url::Url;
+
+pub const E2E_CONFIG: &str = r#"
+    [coordinator]
+    service_name = "mpc-coordinator"
+    db.url = "postgres://postgres:postgres@localhost:8432/coordinator_db"
+    db.migrate = true
+    db.create = true
+    queues.queries_queue_url = "http://localhost:4566/000000000000/coordinator-queries.fifo"
+    queues.distances_queue_url = "http://localhost:4566/000000000000/coordinator-distances.fifo"
+    queues.db_sync_queue_url = "http://localhost:4566/000000000000/coordinator-db-sync"
+    participants = '["0.0.0.0:8080", "0.0.0.0:8081"]'
+    hamming_distance_threshold = 0.375
+
+    [[participant]]
+    socket_addr = "0.0.0.0:8080"
+    batch_size = 20000
+    db.url = "postgres://postgres:postgres@localhost:8433/participant_0_db"
+    db.migrate = true
+    db.create = true
+    queues.db_sync_queue_url = "http://localhost:4566/000000000000/participant-0-db-sync"
+
+    [[participant]]
+    socket_addr = "0.0.0.0:8081"
+    batch_size = 20000
+    db.url = "postgres://postgres:postgres@localhost:8434/participant_1_db"
+    db.migrate = true
+    db.create = true
+    queues.db_sync_queue_url = "http://localhost:4566/000000000000/participant-1-db-sync"
+"#;
+
+#[derive(Debug, Deserialize)]
+struct E2EConfig {
+    coordinator: CoordinatorConfig,
+    participant: Vec<ParticipantConfig>,
+}
 
 #[tokio::test]
-async fn e2e() -> eyre::Result<()> {
+async fn test_e2e() -> eyre::Result<()> {
+    let settings = Config::builder()
+        .add_source(config::File::from_str(
+            E2E_CONFIG,
+            config::FileFormat::Toml,
+        ))
+        .build()?;
+
+    let mut e2e_config = settings.try_deserialize::<E2EConfig>()?;
+
+    println!("Initializing resources");
+    initialize_resources(&mut e2e_config).await?;
+
+    let database = Arc::new(Db::new(&e2e_config.coordinator.db).await?);
+
+    let coordinator = Coordinator::new(e2e_config.coordinator).await?;
+    // let participant_0 =
+    //     Participant::new(e2e_config.participant[0].clone()).await?;
+    // let participant_1 =
+    //     Participant::new(e2e_config.participant[1].clone()).await?;
+
+    //TODO: spin up all of the nodes
+
+    //TODO: run the e2e test
+
+    Ok(())
+}
+
+async fn initialize_resources(e2e_config: &mut E2EConfig) -> eyre::Result<()> {
     let docker = clients::Cli::default();
 
-    // Coordinator
-    let postgres_image = Postgres::default().with_host_auth();
-    let pg_node_coordinator = docker.run(postgres_image);
-    let pg_port_coordinator = pg_node_coordinator.get_host_port_ipv4(5432);
+    // Setup LocalStack
+    let localstack_image = GenericImage::new("localstack/localstack", "latest")
+        .with_env_var("SERVICES", "sqs")
+        .with_exposed_port(4566);
+    let localstack_container = docker.run(localstack_image);
+    let localstack_host_port = localstack_container.get_host_port_ipv4(4566);
 
-    // Paricipant 0
-    let postgres_image = Postgres::default().with_host_auth();
-    let pg_node_participant_0 = docker.run(postgres_image);
-    let pg_port_participant_0 = pg_node_participant_0.get_host_port_ipv4(5432);
+    let sqs_client = sqs_client_from_config(&AwsConfig {
+        endpoint: Some(format!("http://localhost:{:?}", localstack_host_port)),
+        region: None,
+    })
+    .await?;
 
-    // Paricipant 1
-    let postgres_image = Postgres::default().with_host_auth();
-    let pg_node_participant_1 = docker.run(postgres_image);
-    let pg_port_participant_1 = pg_node_participant_1.get_host_port_ipv4(5432);
+    let coordinator_port = Url::parse(&e2e_config.coordinator.db.url)?
+        .port()
+        .expect("Could not extract port");
 
-    let local_stack_node = docker.run(LocalStack);
-    let local_stack_host_port = local_stack_node.get_host_port_ipv4(4566);
+    // Setup databases for Coordinator and Participants
+    let coordinator_db_container =
+        setup_database_container(&docker, coordinator_port).await?;
 
-    let sqs_client = sqs_client_from_config(&mpc::config::AwsConfig { endpoint:None, region:None }).await?;
+    let port = coordinator_db_container.get_host_port_ipv4(coordinator_port);
+
+    e2e_config.coordinator.db.url = format!(
+        "postgres://postgres:postgres@localhost:{}/coordinator_db",
+        port
+    );
+
+    for (i, participant) in e2e_config.participant.iter_mut().enumerate() {
+        let participant_port = Url::parse(&participant.db.url)?
+            .port()
+            .expect("Could not extract port");
+
+        let participant_db_container =
+            setup_database_container(&docker, participant_port).await?;
+
+        let port =
+            participant_db_container.get_host_port_ipv4(participant_port);
+
+        participant.db.url = format!(
+            "postgres://postgres:postgres@localhost:{}/participant_{}_db",
+            port, i
+        );
+    }
 
     let fifo_attributes = {
         let mut map = HashMap::new();
         map.insert(QueueAttributeName::FifoQueue, "true".to_string());
-        map.insert(QueueAttributeName::ContentBasedDeduplication, "true".to_string());
+        map.insert(
+            QueueAttributeName::ContentBasedDeduplication,
+            "true".to_string(),
+        );
         map
     };
 
-    create_queue(&sqs_client, "coordinator-uniqueness-check.fifo", Some(fifo_attributes.clone())).await?;
-    create_queue(&sqs_client, "coordinator-results-queue.fifo", Some(fifo_attributes)).await?;
+    // Create SQS queues
+
+    create_queue(
+        &sqs_client,
+        "coordinator-uniqueness-check.fifo",
+        Some(fifo_attributes.clone()),
+    )
+    .await?;
+    create_queue(
+        &sqs_client,
+        "coordinator-results-queue.fifo",
+        Some(fifo_attributes),
+    )
+    .await?;
     create_queue(&sqs_client, "coordinator-db-sync-queue", None).await?;
     create_queue(&sqs_client, "participant-0-db-sync-queue", None).await?;
     create_queue(&sqs_client, "participant-1-db-sync-queue", None).await?;
 
-
-    // Create coordinator
-
-    Coordinator::new(CoordinatorConfig{
-
-        // should just be a mpc::config::json_wrapper::JsonStrWrapper<Vec<_>>` of the addresses
-      participants: vec!["localhost:5432".to_string(), "localhost:5432".to_string()],
-   
-        db: mpc::config::DbConfig {
-            url: format!("postgres://localhost:{}", pg_port_coordinator),
-            migrate: true,
-            create: true,
-        },
-        
-        hamming_distance_threshold: 0.375,
-        queues: mpc::config::CoordinatorQueuesConfig {
-            queries_queue_url: "https://sqs.us-east-1.amazonaws.com/1234567890/mpc-query-queue".to_string(),
-            distances_queue_url: "https://sqs.us-east-1.amazonaws.com/1234567890/mpc-distance-results-queue".to_string(),
-            db_sync_queue_url: "https://sqs.us-east-1.amazonaws.com/1234567890/mpc-query-queue".to_string(),
-        },
-
-        aws: mpc::config::AwsConfig {
-            endpoint: Some("http://localhost:4566".to_string()),
-            region: None,
-        },
-
-    }).await?;
-
-    // Create participant 0
-
-    // Create participant 1
-
-    // Run signup sequence
-
     Ok(())
+}
+
+async fn setup_database_container(
+    docker: &clients::Cli,
+    port: u16,
+) -> eyre::Result<Container<GenericImage>> {
+    dbg!(port);
+    let db_container = docker.run(
+        GenericImage::new("postgres", "latest")
+            .with_env_var("POSTGRES_HOST_AUTH_METHOD", "trust")
+            .with_exposed_port(port),
+    );
+    Ok(db_container)
 }
 
 async fn create_queue(
@@ -88,7 +178,10 @@ async fn create_queue(
     queue_name: &str,
     attributes: Option<HashMap<QueueAttributeName, String>>,
 ) -> eyre::Result<()> {
-    let  req = sqs_client.create_queue().queue_name(queue_name.to_string())set_attributes(attributes);
+    let req = sqs_client
+        .create_queue()
+        .queue_name(queue_name.to_string())
+        .set_attributes(attributes);
     req.send().await?;
     tracing::info!("Queue created: {}", queue_name);
 
