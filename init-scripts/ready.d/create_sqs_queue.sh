776990bd4eec3b33066913a5fb1624db5b088c71
Dzejkop <jakubtrad@gmail.com>
2024-03-12 17:15:38 +0100

    Fix SQS creation

--- init-scripts/ready.d/create_sqs_queue.sh
+++ init-scripts/ready.d/create_sqs_queue.sh
@@ -2,8 +2,8 @@
 
 # Create your SQS queues
 echo "Creating SQS queues..."
-awslocal sqs create-queue --queue-name coordinator-uniqueness-check.fifo --attributes FifoQueue=true,ContentBasedDeduplication=true
-awslocal sqs create-queue --queue-name coordinator-results-queue.fifo --attributes FifoQueue=true,ContentBasedDeduplication=true
+awslocal sqs create-queue --queue-name coordinator-uniqueness-check.fifo --attributes "{\"FifoQueue\":\"true\", \"ContentBasedDeduplication\":\"true\"}"
+awslocal sqs create-queue --queue-name coordinator-results-queue.fifo --attributes "{\"FifoQueue\":\"true\", \"ContentBasedDeduplication\":\"true\"}"
 
 awslocal sqs create-queue --queue-name coordinator-db-sync-queue
 awslocal sqs create-queue --queue-name participant-0-db-sync-queue
