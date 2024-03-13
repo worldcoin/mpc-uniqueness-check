6fa25abb46fe7738fc95aae2f13c432984bc1e99
Dzejkop <jakubtrad@gmail.com>
2024-03-13 18:55:23 +0100

    Switch to masks

--- bin/utils/measure_fetch.rs
+++ bin/utils/measure_fetch.rs
@@ -5,21 +5,21 @@ use mpc::db::Db;
 #[derive(Debug, Clone, Args)]
 pub struct MeasureFetch {
     #[clap(short, long)]
-    pub participant_db_url: String,
+    pub coordinator_db_url: String,
 }
 
 pub async fn measure_fetch(args: &MeasureFetch) -> eyre::Result<()> {
     let db = Db::new(&DbConfig {
-        url: args.participant_db_url.clone(),
+        url: args.coordinator_db_url.clone(),
         migrate: true,
         create: true,
     })
     .await?;
 
     let start = std::time::Instant::now();
-    let shares = db.fetch_shares(0).await?;
+    let shares = db.fetch_masks(0).await?;
 
-    println!("Shares {} fetched in {:?}", shares.len(), start.elapsed());
+    println!("Masks {} fetched in {:?}", shares.len(), start.elapsed());
 
     Ok(())
 }
