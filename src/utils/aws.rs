3a752292de5a979d50f46b9b733df0b231569f70
0xKitsune <0xKitsune@protonmail.com>
2024-03-19 14:21:57 -0400

    remove queue len check

--- src/utils/aws.rs
+++ src/utils/aws.rs
@@ -37,29 +37,6 @@ pub async fn sqs_client_from_config(
 }
 
 #[tracing::instrument(skip(client, queue_url))]
-pub async fn check_approximate_queue_length(
-    client: &aws_sdk_sqs::Client,
-    queue_url: &str,
-) -> eyre::Result<usize> {
-    let queue_attributes = client
-        .get_queue_attributes()
-        .queue_url(queue_url)
-        .attribute_names(QueueAttributeName::ApproximateNumberOfMessages)
-        .send()
-        .await?;
-
-    let message_count = queue_attributes
-        .attributes()
-        .and_then(|attrs| {
-            attrs.get(&QueueAttributeName::ApproximateNumberOfMessages)
-        })
-        .and_then(|count| count.parse::<usize>().ok())
-        .unwrap_or(0);
-
-    Ok(message_count)
-}
-
-#[tracing::instrument(skip(client, queue_url))]
 pub async fn sqs_dequeue(
     client: &aws_sdk_sqs::Client,
     queue_url: &str,
